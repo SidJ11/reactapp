01396890921b0d824e5734e12337eb8731cc2bfd
Siddharth Joshi <joshi89_siddharth@yahoo.in>
2023-05-03 21:30:45 +0530

    testing

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello from React");
+//const heading = React.createElement("h1", {id:"heading"}, "Hello from React");
+
+const jsxHeading = <h1>This is coming with jsx</h1>
+
+//Functional Component
+const HeadingComponent = () => {
+    return <h1>This heading comes from functional component</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
