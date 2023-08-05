0fcf0ceb6dfe44a7d7d9848d464417fd3ed74a62
Siddharth Joshi <joshi89_siddharth@yahoo.in>
2023-08-05 12:56:14 +0530

    Laying foundation

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//const heading = React.createElement("h1", {id:"heading"}, "Hello from React");
+const Header = () =>{
+    return(
+        <>
+        <div className="header">
+        Logo
+        <br></br>
+        Navbar
+        </div>
+        </>
+    )
+}
 
-//React Element
-const jsxHeading = <h1>This is coming with jsx</h1>
-
-//Functional Component which returns React Element (JSX)
-const HeadingComponent = () => (
-    <><h1>This heading comes from functional component</h1>
-    {jsxHeading}
-    </>
-    
-)
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
